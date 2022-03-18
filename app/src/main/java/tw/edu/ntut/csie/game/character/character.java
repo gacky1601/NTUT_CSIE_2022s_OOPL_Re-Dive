65852034e94cccc826b8c9c68605aabd8e025259
SmallRay <t109000000@ntut.edu.tw>
2022-03-18 10:23:09 +0800

    Character

---
+++ app/src/main/java/tw/edu/ntut/csie/game/character/character.java
@@ -0,0 +1,22 @@
+public abstract class Character {
+    private int _hp;
+    private int _physicalAttack;
+    private int _magicAttack;
+    private int _physicalCritical;
+    private int _magicCritical;
+    private int _physicalDefense;
+    private int _magicDefense;
+    private int _physicalPenetration;
+    private int _magicPenetration;
+    private int _dodge;
+    private int _accuracy;
+    private int _hpRecoveryRate;
+    private int _tpRecoveryRate;
+    private int _waveHpRecovery;
+    private int _waveTpRecovery;
+    private int _hpReduceRate;
+    private int _tpReduceRate;
+    private int _attackRange;
+    private int _moveSpeed;
+    private float _attackSpeed;
+}
