44508c8f796f63d2da8e27d332964cee9c2d6bea
Yupoooo <gacky1601@gmail.com>
2022-04-07 22:37:52 +0800

    2020.4.7 brand new wallpaper

--- app/src/main/java/princess/connect/state/PlayerMenu/charatermap.java
+++ app/src/main/java/princess/connect/state/PlayerMenu/charatermap.java
@@ -31,63 +31,16 @@ public class charatermap implements GameObject {
         return _charater;
     }
 
-    private ArrayList<ArrayList<BitmapButton>> loadallcharaters1(){
-        ArrayList<ArrayList<BitmapButton>> temp=new ArrayList();
-        ArrayList<BitmapButton>  _nc = new ArrayList<>();
-        try {
-            for (String charater:runtime.getAssets().list("character/icon")){
-                Log.d("charater:",charater);
-                _nc.add((new BitmapButton("character/icon/"+charater)));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        int Char_amount= _nc.size(),ncamount=0;
-        ArrayList<BitmapButton> row0=new ArrayList<>();
-        ArrayList<BitmapButton> row1=new ArrayList<>();
-        ArrayList<BitmapButton> row2=new ArrayList<>();
-        for (int i=0 ;i<3;i++){
-            for (int j=0 ;j<3;j++){
-                for(int k = 0; k<5; k++){
-                    if(j==0)
-                        row0.add(_nc.get(ncamount));
-                    if(j==1)
-                        row1.add(_nc.get(ncamount));
-                    if(j==2)
-                        row2.add(_nc.get(ncamount));
-                    Char_amount--;
-                    ncamount++;
-                }
-            }
-            temp.add(row0);
-            temp.add(row1);
-            temp.add(row1);
-            row0.clear();
-            row1.clear();
-            row2.clear();
-        }
-        return temp;
-    }
 
-    public charatermap(){
-//        allcharter=loadallcharaters1();
-    }
+    public charatermap(){}
 
     @Override
     public void move() {}
 
     @Override
     public void show() {
-//        ArrayList<ArrayList<BitmapButton>> _allcharter=new ArrayList();
-//        _allcharter=loadallcharaters1();
         int countw=0,counth=0,w=190,h=80;
-//        for(int i=0;i<4;i++){
-//            for (int j=0;j<5;j++){
-//                allcharter.get(0).get(0).setLocation(w+i*210,h+j*180);
-//                allcharter.get(0).get(0).show();
-//            }
-//        }
         ArrayList <BitmapButton>a=loadallcharaters();
         for(BitmapButton arr :a){
             if (countw>=5){
@@ -101,8 +54,7 @@ public class charatermap implements GameObject {
             arr.show();
             countw++;
         }
-//        _allcharter.get(0).get(0).setLocation(100,100);
-//        _allcharter.get(0).get(0).show();
+
     }
 
 
