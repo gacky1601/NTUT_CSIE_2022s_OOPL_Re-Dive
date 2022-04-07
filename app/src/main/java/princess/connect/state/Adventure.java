b09104f01e83acccb85092015f1ce806b386cb55
SmallRay <t109000000@ntut.edu.tw>
2022-04-07 21:59:07 +0800

    running

--- app/src/main/java/princess/connect/state/Adventure.java
+++ app/src/main/java/princess/connect/state/Adventure.java
@@ -1,7 +1,15 @@
 package princess.connect.state;
 
+import android.util.Log;
+
 import java.util.List;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Handler;
 
 import princeconnect.game.R;
 import princess.connect.Pointer;
@@ -12,7 +20,9 @@ import princess.connect.baseClass.Character;
 import princess.connect.character.*;
 import princess.connect.extend.Animation;
 
-public class Adventure extends AbstractGameState{
+public class Adventure extends AbstractGameState {
+    Timer timer;
+    TimerTask timeTask;
 
     /**
      * 建構一個<code>GameState</code>實體。
@@ -26,24 +36,35 @@ public class Adventure extends AbstractGameState{
 
     @Override
     public void initialize(Map<String, Object> data) {
-        MovingBitmap background = new MovingBitmap(R.drawable.bg_100021,0,-200);
-        background.resize(1920  ,1080);
+        MovingBitmap background = new MovingBitmap(R.drawable.bg_100021, 0, -200);
+        background.resize(1920, 1080);
         addGameObject(background);
         List<Character> characterLeft = List.of(new Pecorine());
         List<Character> characterRight = List.of(new Kokoro());
         BattleGround ground = new BattleGround(characterLeft, characterRight);
         ground.initialize();
         ground.main();
-        Animation animation = getAnimationFromAssets("character/pecorine/idle");
+        Animation animation = getAnimationFromAssets("character/pecorine/run");
         animation.setDelay(1);
-        animation.setLocation(-0,-0);
+        animation.setLocation(-200, 200);
         addGameObject(animation);
+        timer = new Timer();
+        timeTask = new TimerTask() {
+            @Override
+            public void run() {
+                Log.d("TAG", "run: test");
+                animation.setLocation(animation.getX() + 2,animation.getY());
+            }
+        };
+        timer.scheduleAtFixedRate(timeTask,0,5);
     }
 
     @Override
-    public void pause() {    }
+    public void pause() {
+    }
 
     @Override
-    public void resume() {    }
+    public void resume() {
+    }
 
 }
