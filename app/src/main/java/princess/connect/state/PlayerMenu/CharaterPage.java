de4ab5142a70fbbe6ad1785ca82aee043b20b9ae
Yupoooo <gacky1601@gmail.com>
2022-04-10 17:55:05 +0800

    2022.4.10 bugfix

--- app/src/main/java/princess/connect/state/PlayerMenu/CharaterPage.java
+++ app/src/main/java/princess/connect/state/PlayerMenu/CharaterPage.java
@@ -18,15 +18,39 @@ import princess.connect.engine.GameEngine;
 import princess.connect.extend.BitmapButton;
 
 public class CharaterPage extends PlayerMenu {
+
     public CharaterPage(GameEngine engine) { super(engine);}
+    private ArrayList<ArrayList<BitmapButton>> allcharter=new ArrayList();
+    private int _onShowPage=0;
+
     public void initialize(Map<String, Object> data) {
         add();
         MovingBitmap _Charbackground = new MovingBitmap(R.drawable.cha);
         _Charbackground.setLocation(100,50);
         _Charbackground.resize((int) ( _Charbackground.getWidth()*1.3), (int) (_Charbackground.getHeight()*1.3));
         changebg(R.drawable.bg_100941,0,-200,1920,1080);
-//        charatermap cm = new charatermap();
         addGameObject(_Charbackground);
+        showallcharacter();
+
+    }
+
+
+
+    private ArrayList loadallcharaters(){
+        ArrayList<BitmapButton> _charater=new ArrayList();
+        try {
+            for (String charater : runtime.getAssets().list("character")){
+                Log.d("charater:",charater);
+                _charater.add((new BitmapButton("character/"+charater)));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d("Charatersize",String.valueOf(_charater.size()));
+        return _charater;
+    }
+
+    private void showallcharacter(){
         int countw=0,counth=0,w=190,h=80;
         ArrayList <BitmapButton>a=loadallcharaters();
         for(BitmapButton arr :a){
@@ -41,22 +65,6 @@ public class CharaterPage extends PlayerMenu {
             addGameObject(arr);
             countw++;
         }
-    }
-
-    private ArrayList<ArrayList<BitmapButton>> allcharter=new ArrayList();
-    private int _onShowPage=0;
-    private ArrayList loadallcharaters(){
-        ArrayList<BitmapButton> _charater=new ArrayList();
-        try {
-            for (String charater : runtime.getAssets().list("icon")){
-                Log.d("charater:",charater);
-                _charater.add((new BitmapButton("icon/"+charater)));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Log.d("Charatersize",String.valueOf(_charater.size()));
-        return _charater;
     }
 
 }
